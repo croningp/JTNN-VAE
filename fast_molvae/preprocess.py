9d9de9f283d8307983b321c1e937d783383d5b28
Hessam Mehr <hessammehr@gmail.com>
2021-03-07 13:31:14 +0000

    Expose preprocessing tensor pickle generation as a function.

--- fast_molvae/preprocess.py
+++ fast_molvae/preprocess.py
@@ -1,11 +1,5 @@
-import torch
-import torch.nn as nn
-from multiprocessing import Pool
-
-import math, random, sys
 from optparse import OptionParser
 import pickle as pickle
-import os
 from fast_jtnn import *
 import rdkit
 
@@ -24,31 +18,26 @@ def tensorize(smiles, assm=True):
 
     return mol_tree
 
+def create_tensor_pickle(train_path, output_path):
+    lg = rdkit.RDLogger.logger() 
+    lg.setLevel(rdkit.RDLogger.CRITICAL)
+    
+    with open(train_path) as f:
+        data = [line.strip("\r\n ").split()[0] for line in f]
+    print('Input File read')
+    
+    all_data = list(map(tensorize, data))
+
+    with open(output_path, 'wb') as f:
+        pickle.dump(all_data, f, pickle.HIGHEST_PROTOCOL)
+
+
 if __name__ == "__main__":
     lg = rdkit.RDLogger.logger() 
     lg.setLevel(rdkit.RDLogger.CRITICAL)
 
     parser = OptionParser()
     parser.add_option("-t", "--train", dest="train_path")
-    parser.add_option("-n", "--split", dest="nsplits", default=10)
-    parser.add_option("-j", "--jobs", dest="njobs", default=8)
-    opts,args = parser.parse_args()
-    opts.njobs = int(opts.njobs)
+    opts = parser.parse_args()
 
-    pool = Pool(opts.njobs)
-    num_splits = int(opts.nsplits)
-
-    with open(opts.train_path) as f:
-        data = [line.strip("\r\n ").split()[0] for line in f]
-
-    all_data = pool.map(tensorize, data)
-
-    le = (len(all_data) + num_splits - 1) / num_splits
-
-    for split_id in xrange(num_splits):
-        st = split_id * le
-        sub_data = all_data[st : st + le]
-
-        with open('tensors-%d.pkl' % split_id, 'wb') as f:
-            pickle.dump(sub_data, f, pickle.HIGHEST_PROTOCOL)
-
+    create_tensor_pickle(opts.train_path, 'tensors-0.pkl')
