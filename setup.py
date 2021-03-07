cb8db0c7e006e56e9726ff5d23f2997b334a7a4e
Hessam Mehr <hessammehr@gmail.com>
2021-03-07 13:16:37 +0000

    Create package (by adding setup.py)

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='jtnn',
+    version='0.1.0',
+    packages=find_packages(),
+    package_data={
+    },
+    include_package_data=True,
+)
