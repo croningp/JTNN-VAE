6e3eb2b8981e6afc6d8fd0a2d47b2f1790a1cd38
Hessam Mehr <hessammehr@gmail.com>
2021-03-07 13:57:38 +0000

    Expose mol_tree.py's build_vocab as a function.

--- fast_jtnn/mol_tree.py
+++ fast_jtnn/mol_tree.py
@@ -1,18 +1,27 @@
+import logging
+
 import rdkit
 import rdkit.Chem as Chem
-from .chemutils import get_clique_mol, tree_decomp, get_mol, get_smiles, set_atommap, enum_assemble, decode_stereo
+from .chemutils import (
+    get_clique_mol,
+    tree_decomp,
+    get_mol,
+    get_smiles,
+    set_atommap,
+    enum_assemble,
+)
 from .vocab import *
 from tqdm import tqdm
 
-class MolTreeNode(object):
 
+class MolTreeNode(object):
     def __init__(self, smiles, clique=[]):
         self.smiles = smiles
         self.mol = get_mol(self.smiles)
 
-        self.clique = [x for x in clique] #copy
+        self.clique = [x for x in clique]  # copy
         self.neighbors = []
-        
+
     def add_neighbor(self, nei_node):
         self.neighbors.append(nei_node)
 
@@ -25,10 +34,10 @@ class MolTreeNode(object):
 
         for nei_node in self.neighbors:
             clique.extend(nei_node.clique)
-            if nei_node.is_leaf: #Leaf node, no need to mark 
+            if nei_node.is_leaf:  # Leaf node, no need to mark
                 continue
             for cidx in nei_node.clique:
-                #allow singleton node override the atom mapping
+                # allow singleton node override the atom mapping
                 if cidx not in self.clique or len(nei_node.clique) == 1:
                     atom = original_mol.GetAtomWithIdx(cidx)
                     atom.SetAtomMapNum(nei_node.nid)
@@ -41,16 +50,17 @@ class MolTreeNode(object):
             original_mol.GetAtomWithIdx(cidx).SetAtomMapNum(0)
 
         return self.label
-    
+
     def assemble(self):
         neighbors = [nei for nei in self.neighbors if nei.mol.GetNumAtoms() > 1]
-        neighbors = sorted(neighbors, key=lambda x:x.mol.GetNumAtoms(), reverse=True)
+        neighbors = sorted(neighbors, key=lambda x: x.mol.GetNumAtoms(), reverse=True)
         singletons = [nei for nei in self.neighbors if nei.mol.GetNumAtoms() == 1]
         neighbors = singletons + neighbors
 
-        cands,aroma = enum_assemble(self, neighbors)
-        new_cands = [cand for i,cand in enumerate(cands) if aroma[i] >= 0]
-        if len(new_cands) > 0: cands = new_cands
+        cands, aroma = enum_assemble(self, neighbors)
+        new_cands = [cand for i, cand in enumerate(cands) if aroma[i] >= 0]
+        if len(new_cands) > 0:
+            cands = new_cands
 
         if len(cands) > 0:
             self.cands, _ = list(zip(*cands))
@@ -58,39 +68,40 @@ class MolTreeNode(object):
         else:
             self.cands = []
 
-class MolTree(object):
 
+class MolTree(object):
     def __init__(self, smiles):
         self.smiles = smiles
         self.mol = get_mol(smiles)
 
-        #Stereo Generation (currently disabled)
-        #mol = Chem.MolFromSmiles(smiles)
-        #self.smiles3D = Chem.MolToSmiles(mol, isomericSmiles=True)
-        #self.smiles2D = Chem.MolToSmiles(mol)
-        #self.stereo_cands = decode_stereo(self.smiles2D)
+        # Stereo Generation (currently disabled)
+        # mol = Chem.MolFromSmiles(smiles)
+        # self.smiles3D = Chem.MolToSmiles(mol, isomericSmiles=True)
+        # self.smiles2D = Chem.MolToSmiles(mol)
+        # self.stereo_cands = decode_stereo(self.smiles2D)
         self.out_of_vocab = False
         cliques, edges = tree_decomp(self.mol)
         self.nodes = []
         root = 0
-        for i,c in enumerate(cliques):
+        for i, c in enumerate(cliques):
             cmol = get_clique_mol(self.mol, c)
             node = MolTreeNode(get_smiles(cmol), c)
             self.nodes.append(node)
-            if min(c) == 0: root = i
+            if min(c) == 0:
+                root = i
 
-        for x,y in edges:
+        for x, y in edges:
             self.nodes[x].add_neighbor(self.nodes[y])
             self.nodes[y].add_neighbor(self.nodes[x])
-        
-        if root > 0:
-            self.nodes[0],self.nodes[root] = self.nodes[root],self.nodes[0]
 
-        for i,node in enumerate(self.nodes):
+        if root > 0:
+            self.nodes[0], self.nodes[root] = self.nodes[root], self.nodes[0]
+
+        for i, node in enumerate(self.nodes):
             node.nid = i + 1
-            if len(node.neighbors) > 1: #Leaf node mol is not marked
+            if len(node.neighbors) > 1:  # Leaf node mol is not marked
                 set_atommap(node.mol, node.nid)
-            node.is_leaf = (len(node.neighbors) == 1)
+            node.is_leaf = len(node.neighbors) == 1
 
     def size(self):
         return len(self.nodes)
@@ -103,25 +114,45 @@ class MolTree(object):
         for node in self.nodes:
             node.assemble()
 
+
 def dfs(node, fa_idx):
     max_depth = 0
     for child in node.neighbors:
-        if child.idx == fa_idx: continue
+        if child.idx == fa_idx:
+            continue
         max_depth = max(max_depth, dfs(child, node.idx))
     return max_depth + 1
 
 
-if __name__ == "__main__":
-    import sys
-    lg = rdkit.RDLogger.logger() 
-    lg.setLevel(rdkit.RDLogger.CRITICAL)
-
+def build_vocab(input_file, vocab_file, max_tree_width):
     cset = set()
-    for line in sys.stdin:
-        smiles = line.split()[0]
+    for i, line in tqdm(enumerate(input_file.readlines())):
+        smiles = line.strip().split()[0]
+        alert = False
         mol = MolTree(smiles)
         for c in mol.nodes:
+            if c.mol.GetNumAtoms() > max_tree_width:
+                alert = True
             cset.add(c.smiles)
-    for x in cset:
-        print(x)
+        if len(mol.nodes) > 1 and alert:
+            logging.warning("%d-th molecule %s has a high tree-width.\n", i + 1, smiles)
 
+    for x in cset:
+        vocab_file.write(x + "\n")
+
+
+def main_mol_tree(in_fname: str = "", out_fname: str = "", max_tree_width=50):
+    input_file = open(in_fname, "r") if in_fname else sys.stdin
+    vocab_file = open(out_fname, "w") if out_fname else sys.stdout
+
+    with input_file, vocab_file:
+        build_vocab(input_file, vocab_file, max_tree_width)
+
+
+if __name__ == "__main__":
+    import sys
+
+    lg = rdkit.RDLogger.logger()
+    lg.setLevel(rdkit.RDLogger.CRITICAL)
+
+    main_mol_tree()
