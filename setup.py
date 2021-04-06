8ffc00e03487d89b322c21938b4570bfa21d25d0
Hessam Mehr <hessammehr@gmail.com>
2021-04-06 20:28:01 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='jtnn',
-    version='0.1.0',
+    name="jtnn",
+    version="0.2.0",
     packages=find_packages(),
-    package_data={
-    },
+    package_data={},
     include_package_data=True,
 )
