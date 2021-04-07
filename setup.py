cd071273478adbc8804a3f09a99358adf70765ef
Hessam Mehr <hessammehr@gmail.com>
2021-04-07 12:08:53 +0100

    Update installation requirements.

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="jtnn",
     version="0.2.0",
     packages=find_packages(),
     package_data={},
     include_package_data=True,
+    requires=requirements
 )
